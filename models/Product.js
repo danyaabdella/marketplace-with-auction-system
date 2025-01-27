fde047dfc96669918c48569e92bde48d2f335bf7
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2025-01-27 10:25:24 +0300

    Commit

--- models/Product.js
+++ models/Product.js
@@ -50,4 +50,39 @@ ProductSchema.index({ location: "2dsphere" });
 // Create the Product model
 const Product = mongoose.models.Product || mongoose.model("Product", ProductSchema);
 
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    merchantDetail: [
+        {
+            merchantId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+            merchantName: { type: String, required: true } 
+        }
+    ],
+    productName: { type: String, required: true },
+    category: [
+        {
+            categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+            categoryName: { type: String, required: true }
+        }
+    ],
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    description: { type: String, required: true },
+    images: [{ type: String }],
+    productLocation: { type: Object },
+    review: [
+        {
+            customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+            comment: { type: String, required: true },
+            rating: { type: Number, min: 1, max: 5, required: true },
+            createdDate: { type: Date, default: Date.now }
+        }
+    ],
+    delivery: { type: String, enum: ['Standard', 'Express'], required: true },
+    deliveryPrice: { type: Number, required: true }
+});
+
+const Product =  mongoose.models.Product || mongoose.model('Product', productSchema);
+>>>>>>> f68e91ea46010dbc916d322a59641660eeee627c:marketplace-with-auction/models/Product.js
 export default Product;
