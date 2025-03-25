9fa658023dfc5c2fe7086bb82d380581ce71c653
danya <danyaabdella@gmail.com>
2025-03-25 14:14:35 +0300

    added jwt for authentication

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 //http://localhost:3000/api/auth/signin/credentials
 
 import NextAuth from 'next-auth';
+import { signOut as nextAuthSignOut } from "next-auth/react";
 import { options } from './options';
 import { getServerSession } from 'next-auth';
 import User from '@/models/User';
@@ -9,23 +10,32 @@ const handler = NextAuth(options)
 export { handler as GET, handler as POST }
 
 
-  export async function role() {
-      const session = await getServerSession(options);
-      const userEmail = session?.user?.email;
-      if (!userEmail) {
-        return false;
-      }
-      let userInfo = await User.findOne({email: userEmail})
+  export async function role(req, res) {
+      // const session = await getServerSession(options);
+      // const userEmail = session?.user?.email;
+      // if (!userEmail) {
+      //   return false;
+      // }
+      // let userInfo = await User.findOne({email: userEmail})
 
-      console.log("user info to check role: ", userInfo.role);
-      if(!userInfo) {
-        return false;
-      }
+      // console.log("user info to check role: ", userInfo.role);
+      // if(!userInfo) {
+      //   return false;
+      // }
     
-      return userInfo.role;
+      // return userInfo.role;
+      const user = await userInfo(req, res);  // Call the fixed `userInfo` function
+
+      if (!user) {
+        return null;
+      }
+
+      console.log("User role:", user.role);
+      return user.role;
     }
   export async function signOut() {
-    return NextAuth.signOut(options);
+    // return NextAuth.signOut(options);
+    return nextAuthSignOut();
   }
 
 
