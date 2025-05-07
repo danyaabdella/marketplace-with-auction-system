59cb9249ba91756fd0e5936595409804f87b6c4a
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2025-05-07 10:35:28 +0300

    order schema and cart page update

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     domains: [
       "hebbkx1anhila5yf.public.blob.vercel-storage.com", 
-      "firebasestorage.googleapis.com",// Add Firebase Storage domain
+      "firebasestorage.googleapis.com",
       "example.com",
       'images.unsplash.com',
     ], 
